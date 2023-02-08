c46b17f3c21ab603164e1404e570f924bed61e86
Neepsy <eric55664@gmail.com>
2023-02-07 22:43:46 -0600

    Add capture image and fix raycast bug

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,11 +8,13 @@ public class UIManager : MonoBehaviour
     public Healthbar healthbar;
     public Image reticleRing;
     public Image grazeChargeBar;
+    public Image captureImage;
 
     public void Initialize(PlayerController player)
     {
         healthbar.Initialize(player);
         player.SetReticleRing(reticleRing);
         player.SetGrazeChargeBar(grazeChargeBar);
+        player.SetCaptureImage(captureImage);
     }
 }
