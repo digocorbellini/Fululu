e69e0ca15511748f2301835f5de182a0b926ac2f
Isaac Adams <ia81718@gmail.com>
2023-02-07 21:59:34 -0600

    Player weapon resets on death

--- Assets/Scripts/Player/PlayerFireControl.cs
+++ Assets/Scripts/Player/PlayerFireControl.cs
@@ -111,7 +111,7 @@ public class PlayerFireControl : MonoBehaviour
         SwitchWeapon(defaultWeapon);
     }
 
-    public void CaptureAttack()
+    public bool CaptureAttack()
     {
         Debug.Log("Performing capture!");
         Debug.Log("Bounds: " + (captureBounds.center + shootPoint.position) + " Size: " + captureBounds.size / 2.0f);
@@ -125,9 +125,11 @@ public class PlayerFireControl : MonoBehaviour
                 Debug.Log("Found enemy");
                 SwitchWeapon(controller.captureWeapon);
                 Destroy(controller.gameObject);
-                return;
+                return true;
             }
         }
+
+        return false;
     }
 
     public void SwitchWeapon(Weapon wep)
