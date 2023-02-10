04a4fb3d285abd554f9182f9702067ba1b3a306f
Neepsy <eric55664@gmail.com>
2023-02-10 13:10:03 -0600

    Readd all ghost's animations

--- Assets/Enemies/GenericGhosts/States/GenericGhostDeath.cs
+++ Assets/Enemies/GenericGhosts/States/GenericGhostDeath.cs
@@ -13,36 +13,30 @@ public class GenericGhostDeath : GenericGhostState
     {
         aniDone = false;
         timer = duration;
-        if(controller)
-        {
-
-        }
         controller.ani?.Play("Death");
         //controller.audioSource?.PlayOneShot(deathSFX);
     }
 
     public override void run()
     {
-        // TODO: implement ghost animations
+        if (!aniDone)
+        {
+            if (controller.isAnimationDone("Death"))
+            {
+                aniDone = true;
+            }
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
 
-        //if (!aniDone)
-        //{
-        //    if (controller.isAnimationDone("Death"))
-        //    {
-        //        aniDone = true;
-        //    }
-        //}
-        //else
-        //{
-        //    timer -= Time.deltaTime;
-        //}
+        if (timer < 0)
+        {
+            Destroy(controller.gameObject);
+        }
 
-        //if(timer < 0)
-        //{
-        //    Destroy(controller.gameObject);
-        //}
-
-        Destroy(controller.gameObject);
+        // Destroy(controller.gameObject);
     }
 
     public override string getStateName()
