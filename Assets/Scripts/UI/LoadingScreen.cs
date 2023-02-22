85218a093265f16e741af21d25004fb4afa34ea8
Isaac Adams <ia81718@gmail.com>
2023-02-22 17:30:37 -0600

    Start button now switches to Arena scene Loading screen is shown when switching scenes

---
+++ Assets/Scripts/UI/LoadingScreen.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!anim)
+        {
+            anim = GetComponent<Animator>();
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    public IEnumerator Appear()
+    {
+        gameObject.SetActive(true);
+        anim.Play("Appear");
+        yield return new WaitUntil(() => IsAnimationDone("Appear"));
+    }
+
+    public bool IsAnimationDone(string animationName)
+    {
+        return anim.GetCurrentAnimatorStateInfo(0).IsName(animationName) && anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f;
+    }
+
+    public IEnumerator Disappear()
+    {
+        anim.Play("Disappear");
+        yield return new WaitUntil(() => IsAnimationDone("Disappear"));
+        gameObject.SetActive(false);
+    }
+}
