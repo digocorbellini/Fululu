e245fd8a4a5e49cbf7135408f0528f8440bd2114
digocorbellini <rodrigoocpereira@gmail.com>
2023-02-21 23:31:07 -0600

    fixed camera rotation speed

---
+++ Assets/Scripts/Player/PlayerCameraController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.InputSystem;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [Header("Look Sensitivity")]
+    [SerializeField] private float xLookSpeed;
+    [SerializeField] private float xDeadZoneEpsilon = 0.01f;
+    [SerializeField] private float yLookSpeed;
+    [SerializeField] private float yDeadZoneEpsilon = 0.01f;
+    [SerializeField] private bool invertY = true;
+
+    private CinemachineFreeLook cam;
+    private PlayerInput input;
+    private InputAction lookAction;
+
+    private const float Y_MAX = 1.0f;
+    private const float Y_MIN = 0.0f;
+
+    private void Awake()
+    {
+        input = GetComponent<PlayerInput>();
+        cam = GetComponentInChildren<CinemachineFreeLook>();
+
+        lookAction = input.actions["Look"];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 lookInput = lookAction.ReadValue<Vector2>();
+
+        Debug.Log("Look input value: " + lookInput);
+
+        if (Mathf.Abs(lookInput.x) > xDeadZoneEpsilon)
+        {
+            cam.m_XAxis.Value += lookInput.x * xLookSpeed * Time.deltaTime;
+        }
+        
+        if (Mathf.Abs(lookInput.y) > yDeadZoneEpsilon)
+        {
+            float newYVal = cam.m_YAxis.Value;
+            int sign = (invertY) ? -1 : 1;
+            newYVal = Mathf.Clamp(newYVal + (sign * lookInput.y * yLookSpeed * Time.deltaTime), Y_MIN, Y_MAX);
+            cam.m_YAxis.Value = newYVal;
+        }
+    }
+}
