0e94201fcbec491ae25275bd7bb290c90a9cff7c
digocorbellini <rodrigoocpereira@gmail.com>
2023-02-23 18:01:48 -0600

    added mouse sensitivity option in the settings

--- Assets/Scripts/UI/PauseUI.cs
+++ Assets/Scripts/UI/PauseUI.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PauseUI : MonoBehaviour
 {
+    public GameObject SettingsPanel;
+    public GameObject PausePanel;
+
     // Start is called before the first frame update
     public void Show()
     {
         gameObject.SetActive(true);
+
+        // ensure pause panel is the only active panel
+        PausePanel.SetActive(true);
+        SettingsPanel.SetActive(false);
     }
 
     public void Hide()
     {
         gameObject.SetActive(false);
+
+        // ensure pause panel is the only active panel
+        PausePanel.SetActive(true);
+        SettingsPanel.SetActive(false);
     }
 
     public void ResumeGame()
@@ -23,5 +34,17 @@ public class PauseUI : MonoBehaviour
     public void QuitGame()
     {
         GameManager.instance.QuitGame();
+    }
+
+    public void OpenSettings()
+    {
+        PausePanel.SetActive(false);
+        SettingsPanel.SetActive(true);
+    }
+
+    public void OpenPause()
+    {
+        PausePanel.SetActive(true);
+        SettingsPanel.SetActive(false);
     }
 }
