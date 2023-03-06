11ba516182f573be694ab7b460cb951a432bc257
Isaac Adams <ia81718@gmail.com>
2023-03-06 01:48:37 -0600

    Pause menu now works with controller

--- Assets/Scripts/UI/PauseUI.cs
+++ Assets/Scripts/UI/PauseUI.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseUI : MonoBehaviour
 {
     public GameObject PausePanel;
     public GameObject OptionsPanel;
+    public Selectable initialSelection;
+    public CanvasGroup pauseGroup;
+    public CanvasGroup optionsGroup;
 
     // Start is called before the first frame update
     public void Show()
@@ -15,11 +19,15 @@ public class PauseUI : MonoBehaviour
         // ensure only pause panel is active
         PausePanel.SetActive(true);
         OptionsPanel.SetActive(false);
+        pauseGroup.interactable = true;
+
+        initialSelection.Select();
     }
 
     public void Hide()
     {
         gameObject.SetActive(false);
+        pauseGroup.interactable = true;
     }
 
     public void ResumeGame()
@@ -36,11 +44,15 @@ public class PauseUI : MonoBehaviour
     {
         PausePanel.SetActive(false);
         OptionsPanel.SetActive(true);
+        optionsGroup.interactable = true;
+        pauseGroup.interactable = false;
     }
 
     public void BackToPause()
     {
         PausePanel.SetActive(true);
         OptionsPanel.SetActive(false);
+        optionsGroup.interactable = false;
+        pauseGroup.interactable = true;
     }
 }
