61d2a30b9bdb39df4abec2c1a85156cfe0720cbf
Neepsy <eric55664@gmail.com>
2023-03-07 22:34:27 -0600

    Trickle of enemies in the boss fight

--- Assets/Scripts/SpawnBoss.cs
+++ Assets/Scripts/SpawnBoss.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class SpawnBoss : MonoBehaviour
 {
     public GameObject boss;
+    public EnemySpawner bossMinionSpawner;
 
     public GameObject SpawnTheBoss()
     {
-        return Instantiate(boss, transform.position, Quaternion.identity);
+        GameObject b = Instantiate(boss, transform.position, Quaternion.identity);
+
+        if (bossMinionSpawner)
+        {
+            bossMinionSpawner.RegisterToBoss(b.GetComponentInChildren<EntityHitbox>());
+        }
+        return b;
     }
 }
